20362fe6eff6a017e28c33f864b749d565641ccd
RizkyKhapidsyah <RizkyKhapidsyah@RIKY_PC>
2020-09-01 12:30:19 +0700

    Initial commit

---
+++ webkit2/src/com/wilis/webkit2/webkit2.java
@@ -0,0 +1,18 @@
+package com.wilis.webkit2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class webkit2 extends Activity {
+	WebView browser;
+	
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.main);
+		browser=(WebView)findViewById(R.id.webkit);
+		
+		browser.loadUrl("http://nazruddinsyafaat.blogspot.com");
+	}
+}
